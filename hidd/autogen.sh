5ee796a364c8eec9757a7c29c3a8490d82186a1f
Jiri Slaby <jslaby@suse.cz>
2013-03-09 12:03:03 +0100

    fix autotools warnings

--- hidd/autogen.sh
+++ hidd/autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-touch AUTHORS ChangeLog INSTALL NEWS README || exit 1
+touch AUTHORS ChangeLog NEWS README || exit 1
 aclocal || exit 1
 autoconf || exit 1
 automake --add-missing || exit 1
